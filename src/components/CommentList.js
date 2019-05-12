eb5fd2e891d3fb07730c91c173325ab73c0a4755
Darryl <darrylmangibin@gmail.com>
2019-05-12 21:50:49 +0800

    HOC created

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -26,6 +26,7 @@ class CommentList extends React.Component {
 };
 
 const mapStateToProps = (state) => {
+  console.log(state)
   return {
     comments: state.comments
   }
---
+++ src/components/requireAuth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default (ChildComponent) => {
+  class ComposedComponent extends React.Component {
+    render() {
+      return (
+        <ChildComponent />
+      );
+    };
+  };
+  return ComposedComponent
+};
