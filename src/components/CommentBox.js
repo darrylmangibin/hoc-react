bc2a79ceb26071f57c7e7440ac9ef3f3887fc709
Darryl <darrylmangibin@gmail.com>
2019-05-12 17:34:22 +0800

    textarea implematation

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
 class CommentBox extends React.Component {
+  state = {
+    comment: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      comment: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ comment: '' })
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4>Add a Comment</h4>
-        <textarea />
+        <textarea 
+          value={this.state.comment}
+          onChange={this.handleChange}
+        />
         <div>
           <button>Submit Comment</button>
         </div>
